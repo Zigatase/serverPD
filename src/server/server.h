159eff7d33c931012b9f2273bd3512dfcc7008c0
Zigatase <zigatase@gmail.com>
2023-04-09 20:57:01 +0300

    0.0.0.1

---
+++ src/server/server.h
@@ -0,0 +1,11 @@
+#ifndef SERVERPD_SERVER_H
+#define SERVERPD_SERVER_H
+
+#endif //SERVERPD_SERVER_H
+
+#include <iostream>
+#include <string>
+#include <ws2tcpip.h>
+
+void Server();
+void ShowLastError();
