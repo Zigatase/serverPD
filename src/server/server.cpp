e8562e4b8c2de4ed1d36646af91cc42e91e3e83c
Zigatase <zigatase@gmail.com>
2023-04-12 20:56:31 +0300

    0.0.0.1

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -26,11 +26,11 @@ void Server()
 
     // Bind the ip address and port to a socket
     sockaddr_in hint
-            {
-                    hint.sin_family = AF_INET,
-                    hint.sin_port = htons(13254),
-                    static_cast<u_char>(hint.sin_addr.S_un.S_addr = INADDR_ANY) // Could also use inet_pton ....
-            };
+    {
+        hint.sin_family = AF_INET,
+        hint.sin_port = htons(13254),
+        static_cast<u_char>(hint.sin_addr.S_un.S_addr = INADDR_ANY) // Could also use inet_pton ....
+    };
 
     bind(listening, (sockaddr*)&hint, sizeof(hint));
 
